5c3b30f9283d0ad254a16d6329763d5c2a01f90f
sapoKokas <lucasseabra@ua.pt>
2020-10-20 18:04:29 +0100

    Added Authentication

---
+++ bikeTrack/android/app/src/main/kotlin/com/example/bikeTrack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bikeTrack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bikeTrack/lib/models/authentication.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'http_exception.dart';
+
+class Authentication with ChangeNotifier {
+  Future<void> signUp(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAF5EtSI1WAtEDCBU57S3F9crwnszPLbcI';
+
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+      final responseData = json.decode(response.body);
+      //print(responseData);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> logIn(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAF5EtSI1WAtEDCBU57S3F9crwnszPLbcI';
+
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+      final responseData = json.decode(response.body);
+      //print(responseData);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
