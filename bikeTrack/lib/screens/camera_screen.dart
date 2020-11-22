3ba2510a6b0e5caa87ece0a7b1679ac752730489
sapoKokas <lucasseabra@ua.pt>
2020-11-22 00:15:32 +0000

    added camera and access to gallery

--- bikeTrack/lib/screens/camera_screen.dart
+++ bikeTrack/lib/screens/camera_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:async';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -6,10 +8,39 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreen extends State<CameraScreen> {
+  List<CameraDescription> cameras;
+  CameraController controller;
+  bool isReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    setupCameras();
+  }
+
+  Future<void> setupCameras() async {
+    try {
+      cameras = await availableCameras();
+      controller = new CameraController(cameras[0], ResolutionPreset.medium);
+      await controller.initialize();
+    } on CameraException catch (_) {
+      setState(() {
+        isReady = false;
+      });
+    }
+
+    setState(() {
+      isReady = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Camera Screen")),
-    );
+    if (!isReady && !controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller));
   }
 }
--- bikeTrack/lib/screens/gallery_screen.dart
+++ bikeTrack/lib/screens/gallery_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:bikeTrack/services/photo.dart';
+import 'package:bikeTrack/services/dbhelper.dart';
+import 'dart:async';
 
 class GalleryScreen extends StatefulWidget {
   @override
@@ -6,10 +12,90 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreen extends State<GalleryScreen> {
+  //Future<File> imageFile;
+  File _image;
+
+  final picker = ImagePicker();
+  Image image;
+  DBhelper dbhelper;
+  List<Photo> images;
+
+  @override
+  void initState() {
+    super.initState();
+    images = [];
+    dbhelper = DBhelper();
+    refreshImages();
+  }
+
+  // pickImageFromGallery(){
+  //   ImagePicker.pickImage(source: ImageSource.gallery).then((imgFile){
+  //     String imgString = base64Image(imgFile.readAsBytes());
+  //   })
+  // }
+  refreshImages() {
+    dbhelper.getPhotos().then((imgs) {
+      setState(() {
+        images.clear();
+        images.addAll(imgs);
+      });
+    });
+  }
+
+  Future<void> pickImageFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    final bytes = await pickedFile.readAsBytes();
+    if (pickedFile != null) {
+      //_image = File(pickedFile.path);
+      String imgString = base64Encode(bytes);
+      //print(imgString);
+      Photo photo = Photo(0, imgString);
+      dbhelper.save(photo);
+      refreshImages();
+    } else {
+      print('No image selected.');
+    }
+  }
+
+  gridView() {
+    return Padding(
+        padding: EdgeInsets.all(5.0),
+        child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: 1.0,
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+            children: images.map((photo) {
+              return imageFromBase64String(photo.photoName);
+            }).toList()));
+  }
+
+  static Image imageFromBase64String(String base64String) {
+    return Image.memory(
+      base64Decode(base64String),
+      fit: BoxFit.fill,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Gallery Screen")),
+      appBar: AppBar(
+        title: Text("Gallery Screen"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              pickImageFromGallery();
+            },
+          )
+        ],
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[Flexible(child: gridView())],
+      )),
     );
   }
 }
